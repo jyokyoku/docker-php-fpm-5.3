e05cee5e772ba607758b947db3fbf0f1a648f7d8
jyokyoku <jyokyoku@gmail.com>
2020-02-11 23:49:15 +0900

    modify: Updated the command to generate the module list

--- tests/get-modules.sh
+++ tests/get-modules.sh
@@ -6,12 +6,12 @@ set -o pipefail
 
 MODULES="$( docker run --rm -t --entrypoint=php  jyokyoku/php:5.3.3-fpm -m \
 	| grep -vE '(^\[)|(^\s*$)' \
-	| sort -u
+	| sort -u -f
 )"
 
 echo "| Module       | Built-in  |"
 echo "|--------------|-----------|"
 echo "${MODULES}" | while read line; do
-	line="$( echo "${line}" | sed 's/\r//g' | xargs )"
+	line="$( echo "${line}" | sed 's/.$//g' | xargs )"
 	printf "| %-12s | ✔         |\n" "${line}"
 done
